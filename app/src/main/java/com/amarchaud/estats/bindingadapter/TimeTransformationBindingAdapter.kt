b983977ba8070ccecd8e678d8a4da739a901714c
Antoine Marchaud <tonio2835@gmail.com>
2021-01-28 11:54:37 +0100

    update gradle

--- app/src/main/java/com/amarchaud/estats/bindingadapter/TimeTransformationBindingAdapter.kt
+++ app/src/main/java/com/amarchaud/estats/bindingadapter/TimeTransformationBindingAdapter.kt
@@ -1,8 +1,11 @@
 package com.amarchaud.estats.bindingadapter
 
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.amarchaud.estats.utils.TimeTransformation
+import kotlinx.coroutines.MainScope
 
 object TimeTransformationBindingAdapter {
     @JvmStatic
